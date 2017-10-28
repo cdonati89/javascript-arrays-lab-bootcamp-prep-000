8c87f7fceb2f68f0de66a5c7d018f9867a034949
Christopher Donati <cdonati89@gmail.com>
2017-10-28 17:32:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(kitten) {
   kittens.push(kitten);
